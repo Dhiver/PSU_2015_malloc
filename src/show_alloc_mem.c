0ff49b2c34635da256d1204016da19cf65b44d64
Bastien DHIVER <epitech@bastn.fr>
2016-02-01 23:28:30 +0100

    Don't show free blocks in show_alloc_mem()

--- src/show_alloc_mem.c
+++ src/show_alloc_mem.c
@@ -5,7 +5,7 @@
 ** Login   <dhiver_b@epitech.net>
 ** 
 ** Started on  Wed Jan 27 15:48:04 2016 Bastien DHIVER
-** Last update Mon Feb 01 23:06:57 2016 Bastien DHIVER
+** Last update Mon Feb 01 23:26:40 2016 Bastien DHIVER
 */
 
 #include "malloc.h"
@@ -19,6 +19,8 @@ void		show_alloc_mem(void)
   tmp = start_point;
   while (tmp)
     {
+      if (tmp->free && (tmp = tmp->next))
+	continue;
       printf("%p - %p : ", tmp->ptr, (char *)tmp->ptr + tmp->size);
       printf("%ld bytes\n", tmp->size);
       tmp = tmp->next;
