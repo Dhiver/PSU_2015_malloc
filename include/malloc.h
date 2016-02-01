95b1441e58f94b784ddff8cada5ac2451e697d7a
Bastien DHIVER <epitech@bastn.fr>
2016-02-01 16:08:29 +0100

    Fill include/malloc.h

--- include/malloc.h
+++ include/malloc.h
@@ -5,7 +5,7 @@
 ** Login   <dhiver_b@epitech.net>
 ** 
 ** Started on  Wed Jan 27 15:36:19 2016 Bastien DHIVER
-** Last update Wed Jan 27 15:51:38 2016 Bastien DHIVER
+** Last update Mon Feb 01 15:57:15 2016 Bastien DHIVER
 */
 
 #ifndef MALLOC_H_
@@ -13,10 +13,33 @@
 
 # include <unistd.h>
 # include <stdio.h>
+# include "malloc_struct.h"
 
-void		*malloc(size_t);
-void		*realloc(void *, size_t);
-void		free(void *);
-void		show_alloc_mem(void);
+# define META_SIZE	sizeof(struct s_block)
+
+typedef	enum		e_bool
+{
+  FALSE,
+  TRUE
+}			bool;
+
+extern void		*start_point;
+extern void		*end_point;
+
+void			*malloc(size_t);
+/*void			*realloc(void *, size_t);*/
+/*void			free(void *);*/
+/*void			show_alloc_mem(void);*/
+
+bool			check_addr(void *);
+t_block			get_block(void *);
+size_t			get_memory_size(size_t);
+void			set_next_and_prev(t_block);
+/* */
+t_block			create_block(size_t);
+t_block			find_block(t_block *, size_t);
+void			merge_block(t_block);
+void			copy_block(t_block, t_block);
+void			split_block(t_block, size_t);
 
 #endif /* !MALLOC_H_ */
