7c9cdda243c0a73e2d9e7c2c2455cbdf06483c8f
Bastien DHIVER <epitech@bastn.fr>
2016-02-02 16:16:01 +0100

    Add get_memory_size() unit tests

---
+++ tests/test_main.c
@@ -0,0 +1,18 @@
+/*
+** test_main.c for test_main.c in /home/perso/work/projets/malloc_doc/PSU_2015_malloc
+** 
+** Made by Bastien DHIVER
+** Login   <dhiver_b@epitech.net>
+** 
+** Started on  Tue Feb 02 15:49:05 2016 Bastien DHIVER
+** Last update Tue Feb 02 15:57:01 2016 Bastien DHIVER
+*/
+
+#include "test_main.h"
+
+int	main(void)
+{
+  tests_get_memory_size();
+  printf("\033[32;1m OK !\033[0m\n");
+  return (0);
+}
---
+++ tests/test_main.h
@@ -0,0 +1,19 @@
+/*
+** test_main.h for test_main.h in /home/perso/work/projets/malloc_doc/PSU_2015_malloc
+** 
+** Made by Bastien DHIVER
+** Login   <dhiver_b@epitech.net>
+** 
+** Started on  Tue Feb 02 15:49:40 2016 Bastien DHIVER
+** Last update Tue Feb 02 15:53:30 2016 Bastien DHIVER
+*/
+
+#ifndef	TEST_MAIN_H_
+# define TEST_MAIN_H_
+
+# include <assert.h>
+# include "malloc.h"
+
+void		tests_get_memory_size(void);
+
+#endif /* !TEST_MAIN_H_ */
