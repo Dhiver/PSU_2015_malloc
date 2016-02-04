9d2db0631facee8fefec58422085c94bc48ab15b
Bastien DHIVER <epitech@bastn.fr>
2016-02-04 15:37:11 +0100

    Add unit test for tests_create_block

--- tests/tests_create_block.c
+++ tests/tests_create_block.c
@@ -5,7 +5,7 @@
 ** Login   <dhiver_b@epitech.net>
 ** 
 ** Started on  Tue Feb 02 16:40:46 2016 Bastien DHIVER
-** Last update Thu Feb 04 13:45:58 2016 Bastien DHIVER
+** Last update Thu Feb 04 15:28:44 2016 Bastien DHIVER
 */
 
 #include "test_main.h"
@@ -13,17 +13,24 @@
 void		tests_create_block(void)
 {
   t_block	tmp;
+  t_block	tmp2;
+  t_block	tmp3;
   size_t	magic_size;
   t_block	old_end_point;
 
   magic_size = 42;
   old_end_point = end_point;
   tmp = create_block(magic_size);
+  start_point = tmp;
   ast(tmp->size == align_page(align_size(magic_size) + META_SIZE));
   ast(tmp->free == 1);
   ast(tmp->ptr == (char *)tmp + META_SIZE);
   ast(tmp->prev == old_end_point);
   ast(tmp->next == NULL);
   ast(end_point == tmp);
+  tmp2 = create_block(4096);
+  tmp3 = create_block(20);
+  ast(tmp2->next != NULL);
+  (void)tmp3;
   brk(tmp);
 }
