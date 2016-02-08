7e55d444c56866075254dee21859ad6a46744319
Bastien DHIVER <epitech@bastn.fr>
2016-02-08 13:41:03 +0100

    Add merge_block unit tests

--- tests/tests_merge_block.c
+++ tests/tests_merge_block.c
@@ -5,10 +5,19 @@
 ** Login   <dhiver_b@epitech.net>
 ** 
 ** Started on  Wed Feb 03 17:30:43 2016 Bastien DHIVER
-** Last update Thu Feb 04 17:23:43 2016 Bastien DHIVER
+** Last update Mon Feb 08 13:01:13 2016 Bastien DHIVER
 */
 
 #include "test_main.h"
+
+void	tests_merge_block_again(t_block tmp)
+{
+  ast(tmp->size == align_page(align_size(42))
+      + align_page(align_size(21) + META_SIZE)
+      + align_page(align_size(84) + META_SIZE)
+      + align_page(align_size(4096) + META_SIZE)
+      - META_SIZE);
+}
 
 void		tests_merge_block(void)
 {
@@ -26,5 +35,15 @@ void		tests_merge_block(void)
   tmp2->free = 0;
   sav_size = tmp3->size + tmp4->size + META_SIZE;
   merge_block(tmp3);
+  ast(tmp2->next == tmp3);
   ast(tmp3->size == sav_size);
+  ast(tmp3->next == NULL);
+  tmp2->free = 1;
+  merge_block(tmp3);
+  ast(start_point == tmp);
+  ast(end_point == tmp);
+  ast(tmp->free == 1);
+  ast(tmp->prev == NULL);
+  ast(tmp->next == NULL);
+  tests_merge_block_again(tmp);
 }
