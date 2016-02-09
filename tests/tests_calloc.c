302b5aba3a5d71788b9d21d9e405fa41838d8fea
Bastien DHIVER <epitech@bastn.fr>
2016-02-09 12:51:03 +0100

    Add calloc unit test

--- tests/tests_calloc.c
+++ tests/tests_calloc.c
@@ -5,11 +5,18 @@
 ** Login   <dhiver_b@epitech.net>
 ** 
 ** Started on  Mon Feb 08 17:53:17 2016 Bastien DHIVER
-** Last update Mon Feb 08 17:54:32 2016 Bastien DHIVER
+** Last update Tue Feb 09 12:48:21 2016 Bastien DHIVER
 */
 
 #include "test_main.h"
 
-void		tests_calloc(void)
+void	tests_calloc(void)
 {
+  char	*tmp;
+  int	i;
+
+  tmp = calloc(25, sizeof(int));
+  i = 0;
+  while (++i < 25 * (int)sizeof(int))
+    ast(tmp[i] == '\0');
 }
