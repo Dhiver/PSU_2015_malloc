b4edaeb55452675b60a7432676058b0f015d5b86
Bastien DHIVER <epitech@bastn.fr>
2016-02-09 11:16:33 +0100

    Split free()

--- src/free.c
+++ src/free.c
@@ -5,15 +5,36 @@
 ** Login   <dhiver_b@epitech.net>
 ** 
 ** Started on  Wed Jan 27 15:39:37 2016 Bastien DHIVER
-** Last update Tue Feb 09 09:33:55 2016 Bastien DHIVER
+** Last update Tue Feb 09 11:14:32 2016 Bastien DHIVER
 */
 
 #include "malloc.h"
 
+bool		free_at_page_end(t_block tmp)
+{
+  size_t	nb_page;
+
+  nb_page  = get_nb_page(tmp) + 1;
+  if (nb_page * PAGE_SIZE - META_SIZE == tmp->size)
+    {
+      if (tmp->prev)
+	end_point = tmp->prev;
+      else
+	{
+	  end_point = NULL;
+	  brk(start_point);
+	  start_point = NULL;
+	  return (TRUE);
+	}
+    }
+  brk((char*)start_point + (nb_page * PAGE_SIZE));
+  tmp->size -= ((tmp->size / PAGE_SIZE) * PAGE_SIZE);
+  return (FALSE);
+}
+
 void		free(void *ptr)
 {
   t_block	tmp;
-  size_t	nb_page;
 
   if (!ptr || !check_addr(ptr))
     return ;
@@ -22,23 +43,8 @@ void		free(void *ptr)
   tmp->free = 1;
   merge_block(&tmp);
   if (tmp == end_point && tmp->free)
-  {
-    nb_page  = get_nb_page(tmp) + 1;
-    if (nb_page * PAGE_SIZE - META_SIZE == tmp->size)
-    {
-        if (tmp->prev)
-            end_point = tmp->prev;
-        else
-        {
-            end_point = NULL;
-            brk(start_point);
-            start_point = NULL;
-            return ;
-        }
-    }
-    brk((char*)start_point + (nb_page * PAGE_SIZE));
-    tmp->size -= ((tmp->size / PAGE_SIZE) * PAGE_SIZE);
-  }
+    if (free_at_page_end(tmp))
+      return ;
   if (start_point == end_point && ((t_block)end_point)->free)
     {
       brk(start_point);
