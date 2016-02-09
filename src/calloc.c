917d268c01d07444a185fe771d5dfa06d79993da
Bastien DHIVER <epitech@bastn.fr>
2016-02-09 12:22:58 +0100

    Replace bzero() by memset()

--- src/calloc.c
+++ src/calloc.c
@@ -5,22 +5,11 @@
 ** Login   <dhiver_b@epitech.net>
 ** 
 ** Started on  Mon Feb 08 16:57:08 2016 Bastien DHIVER
-** Last update Mon Feb 08 17:45:23 2016 Bastien DHIVER
+** Last update Tue Feb 09 11:31:35 2016 Bastien DHIVER
 */
 
+#include <string.h>
 #include "malloc.h"
-
-void	bzero(void *s, size_t n)
-{
-  char	*ptr;
-
-  ptr = s;
-  while (n != 0)
-    {
-      *ptr++ = 0;
-      n--;
-    }
-}
 
 void		*calloc(size_t nelem, size_t elsize)
 {
@@ -30,6 +19,6 @@ void		*calloc(size_t nelem, size_t elsize)
   sum = nelem + elsize;
   if ((p = malloc(sum)) == NULL)
     return (p);
-  bzero(p, sum);
+  memset(p, '\0', sum);
   return (p);
 }
