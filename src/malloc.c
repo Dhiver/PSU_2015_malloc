94b2e143a17afb5e75059cceaabe181102abe4eb
jarry_m <jarry_m@epitech.eu>
2016-02-10 14:04:21 +0100

    add pThread malloc()

--- src/malloc.c
+++ src/malloc.c
@@ -5,47 +5,44 @@
 ** Login   <dhiver_b@epitech.net>
 ** 
 ** Started on  Wed Jan 27 15:39:00 2016 Bastien DHIVER
-** Last update Tue Feb 09 21:40:56 2016 Bastien DHIVER
+** Last update Wed Feb 10 13:43:05 2016 Jarry Maxime
 */
 
 #include "malloc.h"
 
 void		*start_point = NULL;
 void		*end_point = NULL;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
-void		*malloc(size_t size)
+void		*malloc_func(size_t size)
 {
   t_block	new;
   t_block	last;
 
-  /*printf("malloc(%ld)\n", size);*/
   if (!start_point)
     {
       if (!(new = create_block(size)))
-	{
-	  /*printf("malloc(%ld) return %p\n", size, NULL);*/
-	  /*show_alloc_mem_all();*/
-	  return (NULL);
-	}
+	return (NULL);
       split_block(new, size);
       start_point = new;
       end_point = new->next;
       new->free = 0;
-      /*printf("malloc(%ld) return %p\n", size, new->ptr);*/
-      /*show_alloc_mem_all();*/
       return (new->ptr);
     }
   if (!(new = find_block(&last, size)))
     if (!(new = create_block(size)))
-      {
-	/*printf("malloc(%ld) return %p\n", size, NULL);*/
-	/*show_alloc_mem_all();*/
-	return (NULL);
-      }
+      return (NULL);
   split_block(new, size);
   new->free = 0;
-  /*printf("malloc(%ld) return %p is %d size\n", size, new->ptr, ((char *)new->ptr)[0]);*/
-  /*printf("malloc(%ld) return %p\n", size, new->ptr);*/
-  /*show_alloc_mem_all();*/
   return (new->ptr);
+}
+
+void		*malloc(size_t size)
+{
+  void		*back;
+
+  pthread_mutex_lock(&lock);
+  back = malloc_func(size);
+  pthread_mutex_unlock(&lock);
+  return (back);
 }
